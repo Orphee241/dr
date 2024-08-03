1ce3de5ab1f715f21c02d983d4619fe642c8f359
orphee241 <nzienguiakoumbou@gmail.com>
2024-08-04 00:56:27 +0100

    Public/resources folder and jquery fix

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+/* 
+import "../assets/css/app.min.css" */
+import "../assets/css/bootstrap.min.css"
+
+import "../assets/css/fontawesome.min.css"
+
+import "../assets/css/layerslider.min.css"
+
+import "../assets/css/magnific-popup.min.css"
+
+import "../assets/css/slick.min.css"
+
+import "../css/style.css"
+
+
+
+/* JS */
+
+
+/* import "../assets/js/app.min.js" */
+/* import "../assets/js/vendor/jquery-3.6.0.min.js" */
+import "../assets/js/slick.min.js"
+import "../assets/js/layerslider.utils.js"
+import "../assets/js/layerslider.transitions.js"
+import "../assets/js/layerslider.kreaturamedia.jquery.js"
+import "../assets/js/bootstrap.min.js"
+/* import "../../public/assets/js/wow.min.js" */
+import "../assets/js/jquery.magnific-popup.min.js"
+import "../assets/js/imagesloaded.pkgd.min.js"
+import "../assets/js/isotope.pkgd.min.js"
+import "../assets/js/main.js"
