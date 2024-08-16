8b9871733732b1240ef914af9ff0d52730dc5fcb
orphee241 <nzienguiakoumbou@gmail.com>
2024-08-16 15:42:45 +0100

    Fix css display

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['public/js/app.js', 'public/css/style.css'],
+            input: ['public/js/app.js'],
             refresh: true,
         }),
     ],
