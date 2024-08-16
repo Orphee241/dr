ddc422c916b9b9059b25e67fc4ea091ed179d297
orphee241 <nzienguiakoumbou@gmail.com>
2024-08-16 17:13:20 +0100

    New build

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
 /* 
 import "../assets/css/app.min.css" */
-import "../assets/css/bootstrap.min.css"
+import "../css/bootstrap.min.css"
 
-import "../assets/css/fontawesome.min.css"
+import "../css/fontawesome.min.css"
 
-import "../assets/css/layerslider.min.css"
+import "../css/layerslider.min.css"
 
-import "../assets/css/magnific-popup.min.css"
+import "../css/magnific-popup.min.css"
 
-import "../assets/css/slick.min.css"
+import "../css/slick.min.css"
 
 import "../css/style.css"
 
@@ -19,14 +19,14 @@ import "../css/style.css"
 
 /* import "../assets/js/app.min.js" */
 /* import "../assets/js/jquery-3.6.0.min.js" */
-import "../assets/js/slick.min.js"
-import "../assets/js/layerslider.utils.js"
-import "../assets/js/layerslider.transitions.js"
-import "../assets/js/layerslider.kreaturamedia.jquery.js"
-import "../assets/js/bootstrap.min.js"
+import "./slick.min.js"
+import "./layerslider.utils.js"
+import "./layerslider.transitions.js"
+import "./layerslider.kreaturamedia.jquery.js"
+import "./bootstrap.min.js"
 
-import "../assets/js/jquery.magnific-popup.min.js"
-import "../assets/js/imagesloaded.pkgd.min.js"
-import "../assets/js/isotope.pkgd.min.js"
-import "../assets/js/main.js"
+import "./jquery.magnific-popup.min.js"
+import "./imagesloaded.pkgd.min.js"
+import "./isotope.pkgd.min.js"
+import "./main.js"
 /* import "../../public/assets/js/wow.min.js" */
